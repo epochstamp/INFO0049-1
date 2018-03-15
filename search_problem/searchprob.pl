c8dc0ca91f97ee660f72fe5031468ffe83e0e072
Samy Aittahar <saittahar@localhost.localdomain>
2018-03-15 14:18:41 +0100

    1st version of AI engine in Prolog with a multi-target classic maze

--- search_problem/searchprob.pl
+++ search_problem/searchprob.pl
@@ -4,7 +4,7 @@
 
 
 	### Search problem ###
-        Let n,m € N be the size of a grid G € C^(n,m), with C = {'%','G','F', ' ', '.'} and '.' and 'G' which have exactly one occurence in G.
+        Let n,m € N be the size of a grid G € C^(n,m), with C = {'%','G','F', ' ', '.'} and '.' which have exactly one occurence in G.
 
         State space : 
                       X = {(i,j) € N^2 | for all 0 <= i <= n-1, 0 <= j <= m-1}
@@ -13,34 +13,23 @@
         Transition model : 
                       f((x,y),(i,j)) = (max(0,min(x+i,n-1)), max(0,min(x+j,m-1))) 
                                   for all (x,y) € X, (i,j) € U 
-        Cost model : c((x,y),(i,j)) = 1 for all (x,y) € X, (i,j) € U
+        Cost model : [(x',y') = f((x,y),(i,j))] - c((x,y),(i,j)) = 10 if G[x',y'] = 'G', +inf if G[x',y'] = 'F', 1 otherwise.
         Goal test : 
-		      Given that (x_0,y_0) is NOT the final state,
-                      g((x_0,y_0),(i_0,j_0),...,(i_t,j_t),(x_t+1,y_t+1)) (with t € N the size of the path) = satisfied when (x_t+1,y_t+1) = (X,Y) where G[X,Y] = 'G'
+                      g((x_0,y_0),(i_0,j_0),...,(i_t,j_t),(x_t+1,y_t+1)) is satisfied when 
+                      (i) for all 0 <= k <= t, (x_{k+1},y_{k+1}) = f((x_k,y_k),(i_k,j_k)), 
+                      (ii) for all (x,y) € X, G[x,y] = 'G' => (x,y) is in (x_0,y_0),(i_0,j_0),...,(i_t,j_t),(x_t+1,y_t+1),
+		      and (iii) for all 0 <= k <= t+1, G[x,y] =\/= 'F'  
 
-	Objective : Design an algorithm which find in a minimum amount of time the least-cost path from any initial state which satisfies the goal test	
+	Objective : Design an algorithm which find in a minimum amount of time the least-cost path from any initial state which satisfies the goal test
 
         ### Baseline technique ###
 
-        We can simply use UCS, as seen in the project presentation. While it finds, under some particular assumptions, the optimal path, it only uses the information for the current path at each node.
-        It implies that more nodes than necessary are often explored. We want to reduce this exploration time while ensuring the optimality of the path
+        As seen in the presentation, A* seems to be a good candidate, with the Min Manhattan distance. The Min Manhattan distance does only consider the closest point of interest (here, the 'G's). However, we need our algorithm to scale in big grids. We propose below a modification of the existing A* algorithm with a trade-off between the quality of the solution and the tractability.  
 
 	### Solution ###
-        We can reduce the exploration rate by using A* with an admissible fast-computed heuristic function, such as Manhattan distance (absolute distance between two points), 
-        which is injected as a-priori knowledge of the search problem. As demonstrated in the execution examples (see at the bottom of this file), A* computation time is faster than UCS, as it needs to explore less nodes to find the optimal path.
+	
+	We propose to break up the searching process into several stage. A stage ends when the closest 'G' has been reached. To do so, we choose a node using the Min Manhattan distance
 
-        ### Issues and improvements ###
-	Current implementation of A* won't scale for largely big grids. Even worse, if the grid is big enough, stack overflow can occurs, especially because of Prolog implementation constraints.
-        A solution is to look to 'checkpoints' in order to push away risk of stack overflow. By checkpoints, we means a subset of points for the A* algorithm 
-        to include in the path. including the true final state. Limiting depth of A* is an option, using a more sophisticated heuristics to decide which node we should use to initialize the next
-        execution of A*. For example, it can be a linear combination between the Manhattan heuristics and a serie of path sampling to approximately look to the least-cost path towards the true final state.
-        We do not guarantee anymore the optimality of the path but the solution should be reasonable enough for the decreasing factor of the overall complexity.
-        Greedy solvers of Traveling Salesman Person (cross at most once each cell of the grid before reaching final state) can also be used. Cristofides algorithm (https:\/\/en.wikipedia.org\/wiki\/Christofides_algorithm)
-        is a greedy solver with bounds on graphs which meet the triangle inequality. Once the maze has been preprocessed in a graph, it's very easy to implement that algorithm and give quickly a reasonable solution in average.
-
-        ### How to use it ###
-        1) Declare final state fact like this : final_state(*your final state*)
-        2) Declare forbidden state(s) : forbidden_state(*your forbidden state*)
         
 */
 
@@ -66,7 +55,7 @@ display([L|M]) :- write(L), nl, display(M).
     
 % maze(-M,+F) : Load maze M from file indicated by the file path F
 maze(M,F) :- open(F, read, Stream), read_maze(Stream,L),close(Stream) ->  
-           (convert_to_list_list(L,[[]],MR), reverse_each_list(MR,MR2), reverse(MR2,M)) ; M = [].
+           (convert_to_list_list(L,[[]],MR), reverse_each_list(MR,MR2), myreverse(MR2,M)) ; M = [].
 
 % read_maze(+Stream,-M) - M is the list of characters read from Stream
 
@@ -75,7 +64,7 @@ read_maze(Stream,L) :-
 
 % reverse_each_list(LL,RL) - RL is the list of reversed list of LL
 reverse_each_list([],[]).
-reverse_each_list([LR|MR],[L|M]) :- reverse(LR,L), reverse_each_list(MR,M).
+reverse_each_list([LR|MR],[L|M]) :- myreverse(LR,L), reverse_each_list(MR,M).
 
 % convert_to_list_list(L,M,M2) - M2 is the concatenation of the list of list extracted from string L with \n as separator and M
     
@@ -99,16 +88,49 @@ swap(_,-1,-1,-1,-1,[],[]).
 swap(V,N,K,NP,KP,[L|M],[L|MP]) :- N>0,NP>0,NP2 is NP-1,N2 is N-1,swap(V,N2,K,NP2,KP,M,MP).
 
 
-swap(V,0,K,0,KP,[L|M],[L2|MP]) :- substitute(V,L,K,V2,L3),substitute(V2,L3,KP,V,L2),swap(V,-1,-1,-1,-1,M,MP).
+swap(V,0,K,0,KP,[L|M],[L2|MP]) :- substitute(V,L,K,V2,L3),substitute(V2,L3,KP,_,L2),swap(_,-1,-1,-1,-1,M,MP).
 swap(V,0,K,NP,KP,[L|M],[L2|MP]) :- NP>0,NP2 is NP-1,substitute(V,L,K,V2,L2),swap(V2,-1,-1,NP2,KP,M,MP).
 swap(V,-1,-1,0,KP,[L|M],[L2|MP]) :- substitute(V,L,KP,V2,L2),swap(V2,-1,-1,-1,-1,M,MP).
 
 swap(V,-1,-1,NP,KP,[L|M],[L|MP]) :- NP>0,NP2 is NP-1,swap(V,-1,-1,NP2,KP,M,MP).
 swap(V,-1,-1,-1,-1,[L|M],[L|MP]) :- swap(V,-1,-1,-1,-1,M,MP).
 
+remove(_,[],[]).
+remove(X,[X|L2],L) :- remove(X,L2,L). 
+remove(X,[Y|L2],[Y|L]) :- X\=Y, remove(X,L2,L). 
+
+not_member(_,[]).
+not_member(X,[Y|L]) :- X \= Y; not_member(X,L).
+
+intersect([],_,[]).
+intersect([A|As],Bs,Cs1) :-
+    intersect(As,Bs,Cs), (member(A,Bs) -> Cs1 = [A|Cs], Cs1 = Cs).
+
+fast_intersect([],_,[]).
+fast_intersect(_,[],[]).
+fast_intersect([A|L],[A|L2],[A|L3]) :- fast_intersect(L,L2,L3).
+fast_intersect([A|L],[B|L2],L3) :- B\=A,fast_intersect([A|L],L2,L3).
+
+coords_symbol(M,S,L) :- coords_symbol(M,S,0,[],L).
+
+coords_symbol([],_,_,L,L).
+coords_symbol([R|M],S,N,Lacc,L) :- N2 is N+1, coords_symbol(R,S,N,0,Lacc,L2),coords_symbol(M,S,N2,L2,L).
 
 
+coords_symbol([],_,_,_,L,L).
+coords_symbol([S|R],S,N,M,LAcc,L) :- M2 is M+1, coords_symbol(R,S,N,M2,[[N,M]|LAcc],L).
+coords_symbol([S2|R],S,N,M,LAcc,L) :- S2\=S, M2 is M+1, coords_symbol(R,S,N,M2,LAcc,L).
 
+myreverse(L,R) :- myreverse(L,[],R).
+
+myreverse([],L,L).
+myreverse([A|L], LAcc, L2) :- myreverse(L,[A|LAcc], L2).
+
+flatten_states(L,L2) :- flatten_states(L,[],L2).
+
+flatten_states([],L,L).
+flatten_states([A|L], L2, [A|L3]) :- A = [B,C], integer(B), integer(C), flatten_states(L,L2,L3), !.
+flatten_states([L1|L2], L3, L4) :- flatten_states(L2,L3,L5), flatten_states(L1,L5,L4). 
 /* 
       Implementation of the components of the search problem
 */
@@ -121,26 +143,31 @@ is_action([1,0]).
 is_action([-1,0]).
 
 % Transition cost. In the search problem, a cost of 1 is applied everywhere but here we apply a huge cost to the forbidden state in order to stop exploration from that node.
-transition_cost(S,N) :- final_state(S) -> N = 10000 ; N = 1.
+eval_cell('G',0).
+eval_cell('F',10000).
+eval_cell(C,10) :- not_member(C,['G','F']).
+eval_state(M,[X,Y],N) :- nth0(X,M,L), nth0(Y,L,C), eval_cell(C,N).
 
 
 % is_legal(+[X,Y],+M) - Check if M[X,Y] is not a wall
 is_legal([N,K],M) :- nth0(N,M,RM), nth0(K,RM,C), C \= '%'.
 
 % next_state(+X,+A,+M,-XP,-MP,-C) - Applies action A in state X in grid M after ensuring legality. XP is the next state, MP the updated map and C the cost of the transition 
-next_state(X,A,M,XP,MP,C) :- is_action(A), add_list(X,A,XP), is_legal(XP,M), swap(X,XP,M,MP), transition_cost(MP,C).
+next_state(X,A,M,XP,MP,C) :- is_action(A), add_list(X,A,XP), is_legal(XP,M), eval_state(M,XP,C), swap(X,XP,M,MP).
 
 
 
 
 
-% path_final_state(P) :- Check if the path P ends with the final state, respectively to those who are verified with predicate final_state
-path_final_state([S]) :- final_state(S).
-path_final_state([S|P]) :- P \= [], \+ forbidden_state(S) ,path_final_state(P).
+% path_final_state(P,LSF) : Check if the path P contains all the desired states, at least once.
+path_final_state(P,LSF) :- intersect(LSF,P,LSF).
+
+% fast_path_final_state(P,LSF) : Check if the path P contains all the desired states, at least once. Assume that P and LSF are lexicographically sorted
+fast_path_final_state(P,LSF) :- fast_intersect(LSF,P,LSF).
 
 
 % play_sequence_action(+I,+As,-P,-TC) - Play the sequence of actions As from initial state I.
-play_sequence_action(I,As,P,TC) :- maze(M), get_sequence_state_cost(As,M,[I],0,Ls,TC),reverse(Ls,P),path_final_state(P),write(P). 
+play_sequence_action(I,As,P,TC) :- maze(M), get_sequence_state_cost(As,M,[I],0,Ls,TC),myreverse(Ls,P),path_final_state(P). 
 
 get_sequence_state_cost([],_,Ls,Lc,Ls,Lc).
 get_sequence_state_cost([A|As],M,[S|L],Lcacc,Ls,Lc) :- next_state(S,A,M,SP,MP,C), Lcacc2 is Lcacc + C, get_sequence_state_cost(As,MP,[SP,S|L],Lcacc2,Ls,Lc).
@@ -158,31 +185,42 @@ get_sequence_state_cost([A|As],M,[S|L],Lcacc,Ls,Lc) :- next_state(S,A,M,SP,MP,C)
 
 % Manhattan distance, i.e., |X2 - X1| + |Y2 - Y1|. Final state hard coded.
 % heuristic(+S, -H, +TH) : H is Manhattan Distance between S and final state if TH = 1 otherwise H is 0
-heuristic([X,Y],0,0).
-heuristic([X,Y],H,1) :- final_state([I,J]),A is abs(I - X), B is abs(J - Y), H is A+B.
+m_distance([X,Y],[V,Z],M) :- A is abs(V - X), B is abs(Z - Y), M is A+B. 
+
+min_m_distance(S,L,A) :- min_m_distance(S, L, 100000, A). 
+
+min_m_distance(_,[],M,M).
+min_m_distance(S,[S2|L],A,M) :- m_distance(S,S2,C), (C < A -> min_m_distance(S,L,C,M); min_m_distance(S,L,A,M)).
+
+
+heuristic(_,_,0,0).
+heuristic(S,LSF,1,H) :- min_m_distance(S,LSF,H).
 
 
 % Node expansion
 % expand(+N, +As, +LN2,-LN) : LN is the concatenation of list of nodes expanded from N with actions As and LN2.
-expand([_,_,_,_,_],[],LN,LN).
+expand(_,[],LN,LN).
 expand([X,M,La,P,C],[A|As],LN2,LN) :- next_state(X,A,M,XP,MP,C3) -> C2 is C3+C, expand([X,M,La,P,C],As,[[XP,MP,[A|La],[X|P],C2]|LN2],LN) ; expand([X,M,La,P,C],As,LN2,LN).
 
-% get_final_path_node(+LN, -N) - N is in LN and contains the final state 
-
-get_final_path_node([[X,M,La,P,C] | _],[X,M,La,P,C]) :- final_state(S), P = [S|_].
-get_final_path_node([[_,_,_,P,_] | L],N) :-  final_state(S), \+P = [S|_], get_final_path_node(L,N).
 
 % greedy_get_node(+LN, +HC, +N2, -LN2, -N) - N is N2 or the least cost node of value HC in LN, LN2 is LN without the least cost node of value HC.  
-greedy_get_node([],HN,_,N,[],N).
-greedy_get_node([NC|LN],HN,HC,N2,[N2|LN2],N) :- NC = [X,_,_,_,C], heuristic(X,H,HN), HC2 is H+C, HC2 < HC, greedy_get_node(LN,HC2,NC,LN2,N).
-greedy_get_node([NC|LN],HN,HC,N2,[NC|LN2],N) :- NC = [X,_,_,_,C], heuristic(X,H,HN), HC2 is H+C, HC2 >= HC, greedy_get_node(LN,HC,N2,LN2,N).
+greedy_get_node([],_,_,_,N,_,N).
+greedy_get_node([NC|LN],LSF,HN,H2,N2,[N3|LN2],N) :- NC = [X,_,_,_,C], heuristic(X,LSF,HN,H), HC is H+C, (HC < H2 -> N3 = N2,greedy_get_node(LN,LSF,HN,HC,NC,LN2,N);N3 = NC,greedy_get_node(LN,LSF,HN,H2,N2,LN2,N)) .
+
+
+
+
+multistage_astar(X,[W],M,As,H,PAcc,[P2|PAcc],C2) :- astar(X,[W],M,As,H,P3,W,_,C2), myreverse(P3,P2).
+multistage_astar(X,[W,W2|LSF],M,As,H,PAcc,P,C) :- astar(X,[W,W2|LSF],M,As,H,P3,XF,MF,C2), myreverse(P3,P2), remove(XF,[W,W2|LSF],LSF2), multistage_astar(XF,LSF2,MF,As,H,[P2|PAcc],P,C4), C is C2+C4.
+
+astar(M,As,H,P,C) :- coords_symbol(M,'G',LSF), coords_symbol(M,'.',[X|_]), multistage_astar(X,LSF,M,As,H,[],P2,C), myreverse(P2,P3), flatten_states(P3,P).
+ 
 
 % astar(+X, +M, +As, +H, -P, -C) : P is the optimal path from X to the final state using the components of the search problem described above, given that the used heuristic H is admissible (i.e. between h0 and h*). C is the optimal cost.
-astar(X, M, As, H, P, C) :- astar([[X,M,[],[],0]], As, H, [_,_,_,P2,C]), reverse(P2,P).
+astar(X, LSF, M, As, H, P, XF, MF, C) :- astar_aux([[X,M,[],[],0]], LSF,As, H, [XF,MF,_,P,C]).
 
 % astar(+LN, +As, +H, -NF) - NF is the final node contained in LN. If LN does not contains a final node, the least sum heuristic (H) +cost node is expanded. Fail if there is no final or expandable node in LN.
-astar(LN,As, HN,NF) :- get_final_path_node(LN, N) -> N = NF ; LN = [N2 | LN4], N2 = [X,_,_,_,C],heuristic(X,H,HN), HC is H+C,greedy_get_node(LN4,HN,HC,N2,LN2,N3),expand(N3,As,LN2,LN3), astar(LN3,As,NF).
-
+astar_aux([N2 | LN4],LSF,As, HN,NF) :- N2 = [X,_,_,_,C],heuristic(X,LSF,HN,H), HC is H+C,greedy_get_node(LN4,LSF,HN,HC,N2,LN2,[X2|Xs]), (member(X2,LSF) -> NF = [X2|Xs] ; expand([X2|Xs],As,LN2,LN3), astar_aux(LN3,LSF,As,HN,NF)), !.
 
 /*
 	Execution examples
